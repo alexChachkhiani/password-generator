b7b578f6391f5c36c96436047fd3d0ff2383d706
alexOrdnas <devfterr@gmail.com>
2024-02-27 23:21:58 +0400

    add: working checkboxes

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,20 @@ import './App.css'
 export default function App() {
 
   const [length, setLength] = useState(10)
+  const [checkboxData, setCheckboxData] = useState(
+    [
+      { title: "Include Upperase Letters", state: false },
+      { title: "Include Lowercase Letters", state: false },
+      { title: "Include Numbers", state: false },
+      { title: "Include Symbols", state: false }
+    ]
+  )
 
-  const checkboxData = [
-    { title: "Include Upperase Letters", state: false },
-    { title: "Include Lowercase Letters", state: false },
-    { title: "Include Numbers", state: false },
-    { title: "Include Symbols", state: false }
-  ];
+    const handleCheckboxChange = (index) => {
+      const updatedCheckboxData = [...checkboxData]
+      updatedCheckboxData[index].state = !updatedCheckboxData[index].state
+      setCheckboxData(updatedCheckboxData)
+    }
 
   return (
       <div className="container">
@@ -34,7 +41,7 @@ export default function App() {
         <div className="checkboxes">
           {checkboxData.map((checkbox, index) => {
             return <div key={index}>
-              <input type="checkbox" checked={checkbox.state} />
+              <input type="checkbox" checked={checkbox.state} onChange={() => handleCheckboxChange(index)} />
               <label>{checkbox.title}</label>
             </div>
           })}
